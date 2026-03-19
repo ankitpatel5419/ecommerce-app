634d57681b979d4aba2c4d55bf845245ff62c44b
Ankit Kurmi <ankitpatel57720@gmail.com>
2026-03-19 23:12:11 +0530

    Update backend URL to production environment

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Orders from './pages/Orders'
 import Login from './components/Login'
 import { ToastContainer, toast } from 'react-toastify';
 
-export const backendUrl = import.meta.env.VITE_BACKEND_URL = 'http://localhost:4000'
+export const backendUrl = import.meta.env.VITE_BACKEND_URL = 'https://ecommerce-app-1-727u.onrender.com'
 export const currency = '$'
 
 const App = () => {
