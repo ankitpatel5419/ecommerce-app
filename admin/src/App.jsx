edc515a43d796d53337c1b0c9ec69900bb8f6cff
Ankit Kurmi <ankitpatel57720@gmail.com>
2026-04-05 16:28:06 +0530

    Refactor backend URL to use environment variable
    Updated backend URL assignment to use environment variable.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Orders from './pages/Orders'
 import Login from './components/Login'
 import { ToastContainer, toast } from 'react-toastify';
 
-export const backendUrl = import.meta.env.VITE_BACKEND_URL = 'https://ecommerce-app-1-727u.onrender.com'
+export const backendUrl = import.meta.env.VITE_BACKEND_URL ;
 export const currency = '$'
 
 const App = () => {
